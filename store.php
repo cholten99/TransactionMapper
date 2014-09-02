6fdc974beb554ac5ee3fc77ac8fa46023f11ad91
David Durant <dave@bowsy.co.uk>
2014-09-02 14:16:35 +0100

    First stab at hacky back-end

--- store.php
+++ store.php
@@ -1,24 +1,74 @@
 <?php
 
 // This will only add, update or delete data in the database
+// For a quick demo I'm using a very nasty hack and exposing the data structure 
+// to the front-end to simplify the back-end code a lot
+// action={getall | get | add | update | delete}
+// table={table name}
+// data={getall: | get: id=<id> | add:[var=value] | update: [var=value] && id=<id> |  delete: id=<id>}
 
   $host = getenv("DB1_HOST");
   $user = getenv("DB1_USER");
   $pass = getenv("DB1_PASS");
 
   $mysqli = new mysqli($host, $user, $pass);
-  $mysqli->select_db("plldb");
+  $mysqli->select_db("transaction_mapping");
 
   if ($mysqli->connect_errno) {
     print "Failed to connect to MySQL: " . $mysqli->connect_error;
   }
 
-  $id = $_SESSION['id'];
-  $fetch_string = "SELECT name,location FROM users WHERE (id='" . $id . "')";
-  $result = $mysqli->query($fetch_string);
-  $row = $result->fetch_assoc();
-  $name = $row['name'];
-  $first_name = substr($name, 0, strpos($name, " "));
+  $table = $_GET['table'];
+  unset($_GET['table'];
+  $action = $_GET['action'];
+  unset($_GET['action'];
+  $id = 0;
+  
+  if (($action == 'get') || ($action == 'update') || ($action == 'delete')) {
+    $id = $_GET['id'];
+    unset($_GET['id']; 
+  }
+  
+  $sql_string = "";
+  if ($action == 'getall') {
+    $sql_string = "SELECT * FROM " . $table;
+  } elseif ($action == 'get') {
+    $sql_string = "SELECT * FROM " . $table . " WHERE id=" . $id;
+  } elseif ($action == 'add') {
+    $columns = "";
+    $values = "";
+    foreach ($_GET as $key => $value) {
+      $columns .= "'" . $key . "',";
+      $values .= "'" . $value . "',";
+    }
+    $columns = rtrim($columns, ",");
+    $values = rtrim($values, ",");
+    $sql_string = "INSERT INTO " . $table . "(" . $columns . ") VALUES (" . $values . ")";
+  } elseif ($action == 'update') {
+    $sql_string = "UPDATE " . $table . "SET ";
+    foreach ($_GET as $key => $value) {
+      $sql_string .= "'" . $key . "'='" . $value . "',";
+    }
+    $sql_string = rtrim($sql_string, ",");
+    $sql_string .= "WHERE id=" . $id;
+  } else {
+    $sql_string = "DELETE FROM " . $table . " WHERE id=" . $id;
+  }
+
+  $result = $mysqli->query($sql_string);
+
+  if ($action == 'add') {
+    print $mysqli->insert_id;
+  }
+
+  if (($action == 'getall' || $action == 'get')) {
+    $rows = array();
+    while($row = mysql_fetch_assoc($result)) {
+      $rows[] = $row;
+    }
+    print json_encode($rows);
+  }
+
   $mysqli->close();
 
 ?>
