326b28a46b5393ff45cd61618f37325927848814
David Durant <dave@bowsy.co.uk>
2014-09-02 01:09:34 +0100

    phpmyadmin and other stuff, still can't visualise the layout

--- store.php
+++ store.php
@@ -2,4 +2,23 @@
 
 // This will only add, update or delete data in the database
 
+  $host = getenv("DB1_HOST");
+  $user = getenv("DB1_USER");
+  $pass = getenv("DB1_PASS");
+
+  $mysqli = new mysqli($host, $user, $pass);
+  $mysqli->select_db("plldb");
+
+  if ($mysqli->connect_errno) {
+    print "Failed to connect to MySQL: " . $mysqli->connect_error;
+  }
+
+  $id = $_SESSION['id'];
+  $fetch_string = "SELECT name,location FROM users WHERE (id='" . $id . "')";
+  $result = $mysqli->query($fetch_string);
+  $row = $result->fetch_assoc();
+  $name = $row['name'];
+  $first_name = substr($name, 0, strpos($name, " "));
+  $mysqli->close();
+
 ?>
