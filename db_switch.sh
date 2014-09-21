d0f5738eb0e36269821fff13983fc1a473953f67
David Durant <dave@bowsy.co.uk>
2014-09-21 17:28:10 +0100

    New version of phpMyAdmin

--- db_switch.sh
+++ db_switch.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 if [ "$1" == "dev" ]
 then
-  cp -r phpmyadmin/config.inc.php_dev phpmyadmin/config.inc.php
+  cp -r phpMyAdmin/config.inc.php_dev phpMyAdmin/config.inc.php
   echo "Set up for dev"
 elif [ "$1" == "prod" ]
 then
-  cp -r phpmyadmin/config.inc.php_prod phpmyadmin/config.inc.php
+  cp -r phpMyAdmin/config.inc.php_prod phpMyAdmin/config.inc.php
   echo "Set up for prod"
 else
   echo "Usage ./db_switch.sh [dev | prod]"
